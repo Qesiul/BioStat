a05ba01c5c6abbcca013e24ba33f88b16f496d3d
Luis Wysocki <7wysocki7@gmail.com>
2025-04-03 11:27:37 +0200

    Added test class and method to retrieve date from database and added x name to calendar

---
+++ MVVM/Services/CalendarEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using BioStat.MVVM.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioStat.MVVM.Services;
+
+public class CalendarEventHandler
+{
+    private readonly MeasurmentsTrackerDataAccess _context;
+
+    public CalendarEventHandler(MeasurmentsTrackerDataAccess context)
+    {
+        _context = context;
+    }
+
+    public List<Measurement> GetMeasurementsByDate(DateTime Date)
+    {
+        return _context.Measurements
+            .Where(e => e.Date.Date == Date)
+            .ToList();
+    }
+}
